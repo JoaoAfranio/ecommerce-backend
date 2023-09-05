de1f569e4417c1511f61043628effdf92b1989e8
JoaoAfranio <joaoafraniorosa@hotmail.com>
2023-09-05 19:30:21 -0300

    feat: adding find product function

--- src/repositories/product-repository.ts
+++ src/repositories/product-repository.ts
@@ -1,1 +1,19 @@
+import { Products } from "@prisma/client";
 import { prisma } from "../config";
+
+async function findProductByCode(code: number) {
+  return prisma.products.findFirst({
+    where: {
+      code,
+    },
+    include: {
+      Pack: true,
+    },
+  });
+}
+
+const productRepository = {
+  findProductByCode,
+};
+
+export default productRepository;
