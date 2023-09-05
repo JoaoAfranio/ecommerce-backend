4abb40d89b06cedb08e36ba55f3225f2d167760a
JoaoAfranio <joaoafraniorosa@hotmail.com>
2023-09-05 15:10:46 -0300

    feat: adding get file csv from request

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,3 +1,17 @@
 import { Request, Response } from "express";
+import csvtojson from "csvtojson";
 
-export async function validatePrices(req: Request, res: Response) {}
+export async function validatePrices(req: Request, res: Response) {
+  if (!req.file) {
+    return res.status(400).send("Nenhum arquivo CSV foi enviado.");
+  }
+
+  try {
+    const csvData = req.file.buffer.toString();
+    const csvJson = await csvtojson().fromString(csvData);
+
+    res.status(200).send(csvJson);
+  } catch (error) {
+    console.error("Erro ao converter CSV para JSON:", error);
+  }
+}
--- src/routers/products-router.ts
+++ src/routers/products-router.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { validatePrices } from "../controllers/products-controller";
+import multer from "multer";
 
 const productsRouter = Router();
 
-productsRouter.post("/update-prices", validatePrices);
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+productsRouter.post("/update-prices", upload.single("csvFile"), validatePrices);
 
 export { productsRouter };
